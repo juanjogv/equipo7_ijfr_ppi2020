4be5e76de6a1fd1a82081fac2f73b58987322d99
IzzJuan <juanjosegarcia@inemjose.edu.co>
2020-09-13 18:35:19 -0500

    Mejorando NavBar y cosas varias

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import * as ReactBootStrap from 'react-bootstrap';
+import '../index.css';
+import logo from '../img/r3ai_logo.png';
 
 
 
 
 export default class NavBar extends Component {
+
+
+
     render() {
+        const Style = {
+            background: 'black !important',
+        }
         return (
             <div>
-                <ReactBootStrap.Navbar bg="primary" variant="dark"  >
-                    <ReactBootStrap.Navbar.Brand href="#home"></ReactBootStrap.Navbar.Brand>
+                <ReactBootStrap.Navbar className='color-nav navbar navbar-expand-lg navbar-dark bg-dark fixed-top' style={Style} bg="dark" variant="dark"  >
+                    <ReactBootStrap.Navbar.Brand href="#home"><img src={logo} alt="logo" width="45px" /></ReactBootStrap.Navbar.Brand>
                     <ReactBootStrap.Nav className="mr-auto">
-                        <ReactBootStrap.Nav.Link href='/'>Tareas</ReactBootStrap.Nav.Link>
-                        <ReactBootStrap.Nav.Link href='/mensualidades'>Mensualidad</ReactBootStrap.Nav.Link>
-                        <ReactBootStrap.Nav.Link href='/posts'>Posts</ReactBootStrap.Nav.Link>
+                        <ReactBootStrap.Nav.Link href='/'>Home</ReactBootStrap.Nav.Link>
+                        <ReactBootStrap.Nav.Link href='/mensualidades'>Project</ReactBootStrap.Nav.Link>
+                        <ReactBootStrap.Nav.Link href='/posts'>Team</ReactBootStrap.Nav.Link>
                     </ReactBootStrap.Nav>
                     <ReactBootStrap.Form inline>
                         <ReactBootStrap.FormControl type="text" placeholder="Search" className="mr-sm-2" />
