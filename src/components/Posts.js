1975495b098c46016b33f60caa975cb1f0746573
IzzJuan <juanjosegarcia@inemjose.edu.co>
2020-09-28 16:13:15 -0500

    Implementando posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,43 +1,22 @@
 import React, { Component } from 'react';
-import Post from './Post'
-import Photo from './Photo';
+import posts from '../sample/MOCK_DATA.json';
 
 export default class Posts extends Component {
 
     state = {
-        posts: [],
-        photos: []
+        posts: posts,
     }
-
-
-    async componentDidMount() {
-        const postsAns = await fetch('https://jsonplaceholder.typicode.com/posts');
-        const postsData = await postsAns.json();
-        this.setState({ posts: postsData });
-
-        const photosAns = await fetch('https://jsonplaceholder.typicode.com/photos');
-        const photosData = await photosAns.json();
-        this.setState({ photos: photosData });
-
-    }
-
 
     render() {
         return (
             <div>
-                <h1>Publicaciones & post</h1>
+                <h1>Publicaciones</h1>
                 {
                     this.state.posts.map(post => {
                         return <div key={post.id}>
                             <h1>{post.title}</h1>
                             <p>{post.body}</p>
-                        </div>
-                    })
-                }
-                {
-                    this.state.photos.map(photos => {
-                        return <div key={photos.id}>
-                            <img alt={photos.title} src={photos.url} ></img>
+                            <img src={post.url} alt={post.imageId}></img>
                         </div>
                     })
                 }
