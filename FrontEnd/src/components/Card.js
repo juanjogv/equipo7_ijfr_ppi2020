888bc0a2e7759a295910fa7496e1d44cd349fca7
IzzJuan <juanjosegarcia@inemjose.edu.co>
2020-10-10 12:50:52 -0500

    añadiendo avatares

--- FrontEnd/src/components/Card.js
+++ FrontEnd/src/components/Card.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, Button, Row, Col } from 'react-bootstrap';
 import '../styles/Card.css';
 import ranking from '../sample/ranking.json';
+import { Link } from 'react-router-dom';
 
 class Cards extends React.Component {
     state = {
@@ -10,16 +11,21 @@ class Cards extends React.Component {
     render() {
         return (
             <div >
-                <Row ClassName="d-flex justify-content-center">
+                <Row>
+
+                </Row>
+                <Row className="d-flex justify-content-center">
                     {this.state.ranking.map(e =>
                         <Card as={Col} xl={3} lg={3} md={4} sm={6} xs={10} style={{ width: '18rem' }} className="card">
-                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Img variant="top" src={e.img} />
                             <Card.Body>
-                                <Card.Title>{e.nombre}</Card.Title>
+                                <Card.Title>{e.name}</Card.Title>
                                 <Card.Text>
-                                    puntaje: {e.puntaje}
+                                    puntaje: {e.puesto}
                                 </Card.Text>
-                                <Button variant="primary">Ver</Button>
+                                <Link to="/profile">
+                                    <Button variant="primary">Ver</Button>
+                                </Link>
                             </Card.Body>
                         </Card>
                     )}
--- FrontEnd/src/components/Table.js
+++ FrontEnd/src/components/Table.js
@@ -1,37 +1,39 @@
 import React from 'react';
-import { Table } from 'react-bootstrap';
-import '../styles/Table.css';
+import { Card, Button, Row, Col } from 'react-bootstrap';
+import '../styles/Card.css';
 import ranking from '../sample/ranking.json';
+import { Link } from 'react-router-dom';
 
-class Tables extends React.Component {
+class Cards extends React.Component {
     state = {
         ranking: ranking
     }
-
     render() {
         return (
-            <div className="contenedort">
-                <Table striped bordered hover variant="dark" className="tablat">
-                    <thead>
-                        <tr>
-                            <th>Puesto</th>
-                            <th>Puntos</th>
-                            <th>Nombre</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.ranking.map(ranking =>
-                                <tr key={ranking.id}>
-                                    <td>{ranking.puesto}</td>
-                                    <td>{ranking.puntaje}</td>
-                                    <td>{ranking.nombre}</td>
-                                </tr>
-                        )}
-                    </tbody>
-                </Table>
+            <div >
+                <Row>
+
+                </Row>
+                <Row className="d-flex justify-content-center">
+                    {this.state.ranking.map(e =>
+                        <Card as={Col} xl={3} lg={3} md={4} sm={6} xs={10} style={{ width: '18rem' }} className="card">
+                            <Card.Img variant="top" src={e.img} />
+                            <Card.Body>
+                                <Card.Title>{e.name}</Card.Title>
+                                <Card.Text>
+                                    puntaje: {e.puesto}
+                                </Card.Text>
+                                <Link to="/profile">
+                                    <Button variant="primary">Ver</Button>
+                                </Link>
+                            </Card.Body>
+                        </Card>
+                    )}
+                </Row>
             </div>
+
         );
     }
 }
 
-export default Tables;
+export default Cards;
