ea26ab9631b095ade5c5157fc169804be78eaa54
JuanPabloINEM <juanpariveraalvarez@gmail.com>
2020-11-21 21:11:07 -0500

    Agregamos cards con informacion a la hora de escanear

--- FrontEnd/src/components/QR_Reader.js
+++ FrontEnd/src/components/QR_Reader.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import QrReader from 'react-qr-reader'
 import * as ReactBootStrap from 'react-bootstrap';
 import NavBar2 from '../components/NavBar2';
+import Plastico from '../components/plastico';
+import Papel from '../components/papel';
+import Ordinario from '../components/ordinario';
 
 export default class Test extends Component {
     state = {
@@ -19,6 +22,7 @@ export default class Test extends Component {
         console.error(err)
     }
     render() {
+      if(this.state.result === 'plastico'){
         return (
             <div>
                 <ReactBootStrap.Container>
@@ -33,11 +37,77 @@ export default class Test extends Component {
                             />
                         </ReactBootStrap.Col>
                         <ReactBootStrap.Col className="mt-6 pt-4" xs={12} md={6} >
-                            <p>{this.state.result}</p>
+                                <Plastico/> 
                         </ReactBootStrap.Col>
                     </ReactBootStrap.Row>
                 </ReactBootStrap.Container>
             </div>
         )
+      }else if(this.state.result === 'papel'){
+        return(
+           <div>
+                <ReactBootStrap.Container>
+                    <ReactBootStrap.Row>
+                        <NavBar2 />
+                        <ReactBootStrap.Col xs={12} md={6} >
+                            <QrReader
+                                delay={300}
+                                onError={this.handleError}
+                                onScan={this.handleScan}
+                                style={{ maxWidth: '500px' }}
+                            />
+                        </ReactBootStrap.Col>
+                        <ReactBootStrap.Col className="mt-6 pt-4" xs={12} md={6} >
+                              <Papel/>
+                        </ReactBootStrap.Col>
+                    </ReactBootStrap.Row>
+                </ReactBootStrap.Container>
+            </div>
+        );
+      }else if(this.state.result === 'ordinario'){
+          return(     
+              <div>
+                <ReactBootStrap.Container>
+                    <ReactBootStrap.Row>
+                        <NavBar2 />
+                        <ReactBootStrap.Col xs={12} md={6} >
+                            <QrReader
+                                delay={300}
+                                onError={this.handleError}
+                                onScan={this.handleScan}
+                                style={{ maxWidth: '500px' }}
+                            />
+                        </ReactBootStrap.Col>
+                        <ReactBootStrap.Col className="mt-6 pt-4" xs={12} md={6} >
+                                <Ordinario/>
+                        </ReactBootStrap.Col>
+                    </ReactBootStrap.Row>
+                </ReactBootStrap.Container>
+            </div>
+          );
+      }else{
+        return(
+              <div>
+                <ReactBootStrap.Container>
+                    <ReactBootStrap.Row>
+                        <NavBar2 />
+                        <ReactBootStrap.Col xs={12} md={6} >
+                            <QrReader
+                                delay={300}
+                                onError={this.handleError}
+                                onScan={this.handleScan}
+                                style={{ maxWidth: '500px' }}
+                            />
+                        </ReactBootStrap.Col>
+                        <ReactBootStrap.Col className="mt-6 pt-4" xs={12} md={6} >
+                                <h4> Por favor escanear un codigo QR</h4>
+                        </ReactBootStrap.Col>
+                    </ReactBootStrap.Row>
+                </ReactBootStrap.Container>
+            </div>
+        );
+      }
+      
+
     }
 }
---
+++ FrontEnd/src/components/ordinario.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Card, ListGroup,ListGroupItem, Button} from 'react-bootstrap';
+import ordinario from '../img/ordinarios.png';
+
+export default class Ordinario extends React.Component{
+  render(){
+    return(
+     <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={ordinario}/>
+        <Card.Body>
+          <Card.Title>Ordinario</Card.Title>
+          <Card.Text>
+          </Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroupItem>Estos residuos son los que por lo general se desechan en los domicilios</ListGroupItem>
+          <ListGroupItem>Tambien se pueden catalogar de residuos ordinarios los que son de desecho industrial o de limpieza de areas publicas</ListGroupItem>
+        </ListGroup>
+          <Button target="_blank" href="http://proyectos.andi.com.co/es/GAI/GuiInv/MedAmb/Paginas/GIRS.aspx" variant="success">Mas Informacion</Button> 
+      </Card>
+    );
+  }
+}
+
