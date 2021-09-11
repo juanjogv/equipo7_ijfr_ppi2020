9e2f62c30fa2dc64ffa566b59e80fa07391dc837
Juan Jose Garcia <juangarciavxx@gmail.com>
2021-09-10 19:51:05 -0500

    finalizando implementacion de signin con su controlador

--- Backend/src/lib/interfaces/controllers/authController.js
+++ Backend/src/lib/interfaces/controllers/authController.js
@@ -22,6 +22,20 @@ module.exports = {
   },
 
   async signin(req) {
+    const { nombre_usuario, contrasena_usuario, apellido_usuario, email_usuario } = req.body
 
+    const userCheck = await usuario.findOne({ where: { email_usuario } })
+
+    if (userCheck) throw "ERR_DUP_ENTRY"
+
+    const encrypted_contrasena_usuario = await encryptPassword(contrasena_usuario);
+
+    await usuario.create({
+      nombre_usuario,
+      contrasena_usuario: encrypted_contrasena_usuario,
+      apellido_usuario,
+      email_usuario,
+      escaneos_usuario: 0,
+    });
   }
 }
--- Backend/src/lib/interfaces/routes/autentication.js
+++ Backend/src/lib/interfaces/routes/autentication.js
@@ -4,7 +4,7 @@ const authController = require('../controllers/authController')
 router.post('/login', async (req, res) => {
 	try {
 		await authController.login(req);
-		res.json({ validPass: true });
+		res.json({ valid: true });
 	} catch (error) {
 		if (error === "EMAIL_NOT_FOUND" || error === "EMAIL_AND_PASS_NOT_MATCH") {
 			res.json({
@@ -18,34 +18,19 @@ router.post('/login', async (req, res) => {
 });
 
 router.post("/signin", async (req, res) => {
-	const { nombre_usuario, contrasena_usuario, apellido_usuario, email_usuario } = req.body
-	const newUser = {
-		nombre_usuario,
-		contrasena_usuario,
-		apellido_usuario,
-		email_usuario
-	}
-	const rows = await connection.query('SELECT * FROM usuario where email_usuario = ?', email_usuario);
-
-	if (rows.length > 0) {
-		res.send('false');
-	} else {
-		newUser.contrasena_usuario = await helpers.encryptPassword(contrasena_usuario);
-		await connection.query('INSERT INTO usuario set ?', [newUser]);
+	try {
+		await authController.signin(req);
+		res.json({ valid: true });
+	} catch (error) {
+		if (error === "ERR_DUP_ENTRY") {
+			res.json({
+				valid: false,
+				msg: error,
+			})
+		} else {
+			console.log(error);
+		}
 	}
 });
-
-router.get("/signin/:email", async (req, res) => {
-	const { email } = req.params
-
-	const rows = await connection.query('SELECT * FROM usuario WHERE email_usuario = ?', email);
-
-	if (rows.length > 0) {
-		res.json({ boolean: false });
-	} else {
-		res.json({ boolean: true });
-	}
-});
-
 
 module.exports = router;
