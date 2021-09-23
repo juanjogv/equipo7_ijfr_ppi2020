3928fae6c06173564031f91c32d528f681b615e8
Juan Jose Garcia <juangarciavxx@gmail.com>
2021-09-22 20:37:29 -0500

    modificando el query de ranking para que se ordene de manera descendente

--- Backend/src/lib/interfaces/controllers/rankingController.js
+++ Backend/src/lib/interfaces/controllers/rankingController.js
@@ -2,8 +2,8 @@ const { usuario } = require("../../infrastructure/relationModels");
 module.exports = {
   async getRanking() {
     return await usuario.findAll({
-      attributes: ["nombre_usuario", "apellido_usuario", "escaneos_usuario"],
-      order: ["escaneos_usuario"],
+      attributes: ["nombre_usuario", "apellido_usuario", "foto_usuario", "escaneos_usuario"],
+      order: [["escaneos_usuario", "DESC"]],
     });
   },
 };
